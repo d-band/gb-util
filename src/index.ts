7ed357e3193b58aaf9f0244c75c57f4f6de0cdca
helloyou2012 <helloyou2012@yeah.net>
2021-03-16 12:47:44 +0800

    versin 1.0.1

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export * as GB_11643 from './GB_11643';
 export * as GB_11714 from './GB_11714';
 export * as GB_32100 from './GB_32100';
-export * as GB_T_2260 from './GB_T_2260';
 export * as GB_T_17710 from './GB_T_17710';
+export * as GB_T_2260 from './GB_T_2260';
+export * as GB_T_5795 from './GB_T_5795';
 export * as ISO_7064 from './ISO_7064';
